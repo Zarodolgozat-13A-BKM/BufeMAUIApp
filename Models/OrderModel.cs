85d38eccdc32cd1fb6d666f88fc638897987b0a2
Balinto <balintm2006@gmail.com>
2026-04-02 17:24:37 +0200

    Add OrderModel and OrderItemModel for order data structure

---
+++ Models/OrderModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BufeApp.Models
+{
+    public class OrderModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("order_identifier_number")]
+        public int OrderIdentifierNumber { get; set; }
+
+        [JsonPropertyName("user_username")]
+        public string UserUsername { get; set; }
+
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+
+        [JsonPropertyName("delivery_date")]
+        public string DeliveryDate { get; set; }
+
+        [JsonPropertyName("items")]
+        public List<OrderItemModel> Items { get; set; }
+
+        [JsonPropertyName("total_price")]
+        public string TotalPrice { get; set; }
+
+        [JsonPropertyName("default_completion_time")]
+        public string DefaultCompletionTime { get; set; }
+
+        [JsonPropertyName("comment")]
+        public string Comment { get; set; }
+
+        [JsonPropertyName("payment_intent_id")]
+        public string PaymentIntentId { get; set; }
+
+        public string FormattedDate => DateTime.TryParse(DeliveryDate, out var dt)
+            ? dt.ToString("yyyy. MM. dd.")
+            : DeliveryDate;
+    }
+
+    public class OrderItemModel
+    {
+        [JsonPropertyName("item_id")]
+        public int ItemId { get; set; }
+
+        [JsonPropertyName("item_name")]
+        public string ItemName { get; set; }
+
+        [JsonPropertyName("item_price")]
+        public string ItemPrice { get; set; }
+
+        [JsonPropertyName("picture_url")]
+        public string PictureUrl { get; set; }
+
+        [JsonPropertyName("quantity")]
+        public string Quantity { get; set; }
+
+        [JsonPropertyName("price")]
+        public string Price { get; set; }
+    }
+}
