ee74166d364ddbc209503d0f624d85a9d3cbc06e
Balinto <balintm2006@gmail.com>
2026-04-18 12:10:35 +0200

    Add pagination to orders list on ProfilePage
    Implemented pagination controls and logic for the user's order list on the ProfilePage. Updated the UI to show navigation controls when multiple pages are available. Refactored order loading to use a paginated API response and added new data models for paginated results. The page now loads and displays orders with pagination, improving usability for users with many orders.

---
+++ Models/PaginatedResponseModel.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace BufeApp.Models
+{
+    public class PaginatedResponse<T>
+    {
+        [JsonPropertyName("data")]
+        public List<T> Data { get; set; }
+
+        [JsonPropertyName("meta")]
+        public PaginationMeta Meta { get; set; }
+
+        [JsonPropertyName("links")]
+        public PaginationLinks Links { get; set; }
+    }
+
+    public class PaginationMeta
+    {
+        [JsonPropertyName("current_page")]
+        public int CurrentPage { get; set; }
+
+        [JsonPropertyName("last_page")]
+        public int LastPage { get; set; }
+
+        [JsonPropertyName("total")]
+        public int Total { get; set; }
+
+        [JsonPropertyName("per_page")]
+        public int PerPage { get; set; }
+
+        [JsonPropertyName("from")]
+        public int From { get; set; }
+
+        [JsonPropertyName("to")]
+        public int To { get; set; }
+    }
+
+    public class PaginationLinks
+    {
+        [JsonPropertyName("first")]
+        public string First { get; set; }
+
+        [JsonPropertyName("last")]
+        public string Last { get; set; }
+
+        [JsonPropertyName("prev")]
+        public string Prev { get; set; }
+
+        [JsonPropertyName("next")]
+        public string Next { get; set; }
+    }
+}
